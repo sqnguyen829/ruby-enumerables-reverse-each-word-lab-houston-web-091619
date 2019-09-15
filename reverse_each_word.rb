d73cc02cc84529b1432ac19c03f2e420d491fae4
steven nguyen <sqnguyen829@gmail.com>
2019-09-15 01:54:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,39 @@
-  
-def reverse_each_word(sentence)
-  sentence.split.collect {|word| word.reverse}.join(" ")
-end
+
+
+def reverse_each_word(reverse_this)
+  reverse_this.split.collect {|string| string.reverse}.join(" ")
+end 
+
+
+
+#new_a = reverse_this.reverse
+  #p new_a.split
+  #p new_a.split.reverse.join(" ")
+  #reverse_this.reverse.split.reverse.join(" ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
